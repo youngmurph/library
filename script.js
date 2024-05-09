6bf83dc14eaa74e91dc5d1e15187dac9b7f544e4
Conor Murphy <conormurphy@gmx.de>
2024-05-09 18:48:14 -0400

    Create submit button event listener which uses Book constructor to create book object and push to myLibrary

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+const addBtn = document.getElementById("add-button");
+const dialog = document.querySelector("dialog");
+const closeBtn = document.getElementById("cancel-button");
+const submitBtn = document.getElementById("submit-button");
+
+addBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+    dialog.close();
+});
+
+submitBtn.addEventListener("click", () => {
+    let author = document.getElementById('author').value;
+    let title = document.getElementById('title').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.querySelector('#read');
+    if (read.checked) {
+        read = 'Read';
+    } else {
+        read = 'Unread';
+    };
+    let newBook = new Book(author, title, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
+})
+
 const myLibrary = [];
 
 function Book(author, title, pages, read) {
@@ -8,5 +36,5 @@ function Book(author, title, pages, read) {
 };
 
 function addBook() {
-
+    
 };
