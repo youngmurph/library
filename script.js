8188ce5fd408b8ade872b5d34736cfaa977e0f01
Conor Murphy <conormurphy@gmx.de>
2024-05-09 20:10:29 -0400

    Style modals, add text content

--- script.js
+++ script.js
@@ -49,15 +49,42 @@ function addBook() {
     card.setAttribute("class", "card");
     library.appendChild(card);
 
+    let authLine = document.createElement("p");
+    authLine.setAttribute("class", "modal-text");
+    authLine.textContent = 'Author: ';
+    let titleLine = document.createElement("p");
+    titleLine.setAttribute("class", "modal-text");
+    titleLine.textContent = 'Title: ';
+    let pagesLine = document.createElement("p");
+    pagesLine.setAttribute("class", "modal-text");
+    pagesLine.textContent = 'Pages: ';
+    let checkLine = document.createElement("p");
+    checkLine.setAttribute("class", "modal-text");
+    checkLine.textContent = 'Read: ';
+
+    let details = document.createElement("div");
+    details.setAttribute("class", "modal-details");
+
+    details.appendChild(authLine);
+    details.appendChild(titleLine);
+    details.appendChild(pagesLine);
+    details.appendChild(checkLine);
+
+    let btnDiv = document.createElement("div");
+    btnDiv.setAttribute("class", "btn-div");
+
     let markRead = document.createElement("button");
     markRead.setAttribute("class", "marked");
     markRead.textContent = 'Read';
-    card.appendChild(markRead);
+    btnDiv.appendChild(markRead);
 
     let remove = document.createElement("button");
     remove.setAttribute("class", "remove");
     remove.textContent = 'Remove';
-    card.appendChild(remove);
+    btnDiv.appendChild(remove);
+
+    card.appendChild(details);
+    card.appendChild(btnDiv);
 
     myLibrary.push(newBook);
 };
